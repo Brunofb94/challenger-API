8743ef55e2ba6cabccd6190a38507fcaf2800dcc
Brunofb94 <brunosantiago.a@outlook.com>
2022-04-04 06:38:53 -0300

    m

--- TestBankAccounts/CreateAccount.cs
+++ TestBankAccounts/CreateAccount.cs
@@ -4,12 +4,25 @@ namespace TestBankAccounts
     public class CreateAccount
     {
         [Fact]
-        public void AdicionarNovaContaTestandoInputs()
+        public void AdicionarValorAConta()
         {
-          //Moq.Mock<BankAccounts.Controllers.ContaCorrenteController> mock = new Moq.Mock<BankAccounts.Controllers.ContaCorrenteController>();
-          
-          //var dados = new BankAccounts.Controllers.ContaCorrenteController();
-        
+            var ct = new BankAccounts.Service.AccountService();
+            ct.Depositar(4500, 7);
+
+        }
+        [Fact]
+        public void SacarValorInsuficiente()
+        {
+            var ct = new BankAccounts.Service.AccountService();
+            var sacar = ct.RetirarGrana(45000, 7);
+            Assert.Equal("Saldo Insuficiente!", "Não há Saldo");
+        }
+        [Fact]
+        public void SacarValorSuficiente()
+        {
+            var ct = new BankAccounts.Service.AccountService();
+            var sacar = ct.RetirarGrana(450, 7);
+            Assert.Equal("Valor Retirado!", "Não há Saldo");
         }
     }
 }
